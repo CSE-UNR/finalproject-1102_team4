2084866a66b9803201c321c4fc1eab0b903d01b7
1102-boubaker-wiem <boubakerwiem@gmail.com>
2024-04-30 01:49:28 +0000

    final commit

--- imagecrop.c
+++ imagecrop.c
@@ -1,8 +1,33 @@
 //Name: Vansh Sachdeva, Wiem Boubaker
 //Due date: 05/07/2024
 //Purpose: Image Cropping
+#include <stdio>
+
+#define NUM_ROW 512 
+#define NUM_COLS 384
+#define FileName "Image.txt"
+
+void loadImage( int image [] [cols], int row, int cols, FILE *fp)
+void displayImage( int image [] [cols], int row, int cols, char image [] [cols])
+int cropImage (int image [] [cols], int startRow, int startCols, int endRow, int endCols)
+int dimImage (int image [] [cols], int row, int cols)
+int brightenImage (int image [] [cols], int row, int cols)
+int rotateImage (int image [] [cols], int row, int cols)
+
 
 int main(){
+
+int choice ; 
+int image [][];
+FILE *fp;
+
+printf("1:load an image\n");
+printf("2:Display  \n");
+printf("3:Edit \n");
+printf("4:Exit \n");
+
+
+
 
 	return 0;
 }
